6ccbec3c4a9a09bfe0eedc6119fdb91fc3063efc
Roopesh Shenoy <shenoy.roopesh@gmail.com>
2012-11-26 20:01:12 +0530

    Adding a 3rd challenge

---
+++ NUnitExercises/NUnitExercises/Level1/Challenge3.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using CodeGuru.Exercises;
+
+namespace NUnitExercises.Level1
+{
+    public class Challenge3 : NUnitChallenge, IChallenge
+    {
+        #region constants
+        public const string InitialClass = @"
+public class Person 
+{
+    public string FirstName { get; set; }
+    public string SecondName { get; set; }
+
+    public Person(string name)
+    {
+        var names = name.Split(' ');
+        FirstName = names[0];
+        SecondName = names[1];
+    }
+}
+        ";
+
+
+        public const string InitialTestValue = @"
+using NUnit.Framework;
+
+[TestFixture]
+public class PersonTest
+{
+    [Test]
+    public void CheckConstructor()
+    {
+        var me = new Person(""Roopesh Shenoy"");
+        Assert.AreEqual(""Roopesh"", me.FirstName);
+        Assert.AreEqual(""Shenoy"", me.SecondName);
+    }
+
+    [Test]
+    public void CheckConstructorWithoutSpace()
+    {
+        var me = new Person(""Roopesh"");
+        Assert.AreEqual(""Roopesh"", me.FirstName);
+        Assert.AreEqual("""", me.SecondName);
+    }
+}
+";
+        #endregion
+
+        public Challenge3()
+        {
+            Instructions = "Modify the below class to ensure that all the defined tests pass";
+            CodeFiles.Add(new CodeFile
+                {
+                    Order = 1,
+                    Editable = true,
+                    InitialText = InitialClass,
+                    FileName = "Person.cs"
+                });
+
+            CodeFiles.Add(new CodeFile
+                {
+                    Order = 2,
+                    Editable = false,
+                    InitialText = InitialTestValue,
+                    FileName = "PersonTest.cs"
+                });
+        }
+        
+        /// <summary>
+        /// Validates the Challenge1 user input
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        public string Validate(List<string> code)
+        {
+            try
+            {
+                //check good case
+                code.Add(InitialTestValue);
+                var result = RunTests(code);
+                if (!result.IsSuccess)
+                    return result.Message ?? "Check if the bug is fixed properly";
+
+                //reaches here, means code is fine!
+                return string.Empty;
+
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
---
+++ NUnitExercises/NUnitExercisesTests/Level1/Challenge3Tests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using CodeGuru.Exercises;
+using NUnit.Framework;
+using System.Linq;
+using NUnitExercises.Level1;
+
+namespace NUnitExercisesTests.Level1
+{
+    public class Challenge3Tests
+    {
+        private IChallenge _sut;
+
+        private const string ProperInput = @"
+public class Person 
+{
+    public string FirstName { get; set; }
+    public string SecondName { get; set; }
+
+    public Person(string name)
+    {
+        var names = name.Split(' ');
+        FirstName = names[0];
+        SecondName = names.Length > 1 ? names[1] : """";
+    }
+}
+";
+
+        private const string ImproperInput = @"
+public class Person 
+{
+    public string FirstName { get; set; }
+    public string SecondName { get; set; }
+
+    public Person(string name)
+    {
+        var names = name.Split(' ');
+        FirstName = names[0];
+        SecondName = names.Length > 1 ? names[1] : ""Something"";
+    }
+}
+";
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = new Challenge3();
+        }
+
+        [Test]
+        public void CheckProperInput()
+        {
+            var result = _sut.Validate(new[] {ProperInput}.ToList());
+            Console.WriteLine("result: {0}", result);           
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [TestCase("")]
+        [TestCase(ImproperInput)]
+        [TestCase(Challenge3.InitialClass)]
+        public void CheckInvalidInput(string input)
+        {
+            var result = _sut.Validate(new[] {input}.ToList());
+            Console.WriteLine(input);
+            Console.WriteLine("result: {0}", result);
+            Assert.IsNotNullOrEmpty(result);
+        }
+    }
+}
